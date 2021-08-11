0a50be449040bf11db85478d1390aae9b03563b1
Grzegorz Wielgosinski <g.wielgosins@samsung.com>
2021-08-11 12:23:04 +0200

    Prepare k8s-plugin client for health check and simple status check Issue-ID: SO-3690
    Signed-off-by: Grzegorz Wielgosinski <g.wielgosins@samsung.com>
    Change-Id: I3284ef528ec2bad20adc6ba3ad88fcf4fad715ab

---
+++ so-cnf-adapter-application/src/main/java/org/onap/so/adapters/cnf/client/MulticloudClient.java
@@ -0,0 +1,129 @@
+package org.onap.so.adapters.cnf.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.onap.so.adapters.cnf.MulticloudConfiguration;
+import org.onap.so.adapters.cnf.model.healthcheck.K8sRbInstanceHealthCheck;
+import org.onap.so.adapters.cnf.model.healthcheck.K8sRbInstanceHealthCheckSimple;
+import org.onap.so.adapters.cnf.model.statuscheck.K8sRbInstanceStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.*;
+
+@Component
+public class MulticloudClient {
+
+    private static final Logger log = LoggerFactory.getLogger(MulticloudClient.class);
+
+    private final RestTemplate restTemplate;
+    private final MulticloudConfiguration multicloudConfiguration;
+    private final ObjectMapper objectMapper;
+
+    public MulticloudClient(RestTemplate restTemplate, MulticloudConfiguration multicloudConfiguration) {
+        this.restTemplate = restTemplate;
+        this.multicloudConfiguration = multicloudConfiguration;
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public K8sRbInstanceStatus getInstanceStatus(String instanceId) {
+        MulticloudApiUrl multicloudApiUrl = new MulticloudApiUrl(multicloudConfiguration);
+        multicloudApiUrl.setInstanceId(instanceId);
+        String endpoint = multicloudApiUrl.apiUrl() + "/status";
+        ResponseEntity<String> result  = restTemplate.exchange(endpoint, GET, getHttpEntity(), String.class);
+        log.info("getInstanceStatus response status: {}", result.getStatusCode());
+        String body = result.getBody();
+        log.debug("getInstanceStatus response body: {}", body);
+
+        try {
+            return objectMapper.readValue(body, K8sRbInstanceStatus.class);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public K8sRbInstanceHealthCheckSimple startInstanceHealthCheck(String instanceId) {
+        MulticloudApiUrl multicloudApiUrl = new MulticloudApiUrl(multicloudConfiguration);
+        multicloudApiUrl.setInstanceId(instanceId);
+        String endpoint = multicloudApiUrl.apiUrl() + "/healthcheck";
+        ResponseEntity<String> result  = restTemplate.exchange(endpoint, POST, getHttpEntity(), String.class);
+        log.info("startInstanceHealthCheck response status: {}", result.getStatusCode());
+        String body = result.getBody();
+        log.debug("startInstanceHealthCheck response body: {}", body);
+
+        try {
+            return objectMapper.readValue(body, K8sRbInstanceHealthCheckSimple.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public K8sRbInstanceHealthCheck getInstanceHealthCheck(String instanceId, String healthCheckInstance) {
+        MulticloudApiUrl multicloudApiUrl = new MulticloudApiUrl(multicloudConfiguration);
+        multicloudApiUrl.setInstanceId(instanceId);
+        String endpoint = multicloudApiUrl.apiUrl() + "/healthcheck/" + healthCheckInstance;
+        ResponseEntity<String> result  = restTemplate.exchange(endpoint, GET, getHttpEntity(), String.class);
+        log.info("getInstanceHealthCheck response status: {}", result.getStatusCode());
+        String body = result.getBody();
+        log.debug("getInstanceHealthCheck response body: {}", body);
+
+        try {
+            return objectMapper.readValue(body, K8sRbInstanceHealthCheck.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteInstanceHealthCheck(String instanceId, String healthCheckInstance) {
+        MulticloudApiUrl multicloudApiUrl = new MulticloudApiUrl(multicloudConfiguration);
+        multicloudApiUrl.setInstanceId(instanceId);
+        String endpoint = multicloudApiUrl.apiUrl() + "/healthcheck/" + healthCheckInstance;
+        ResponseEntity<String> result  = restTemplate.exchange(endpoint, DELETE, getHttpEntity(), String.class);
+        log.info("deleteInstanceHealthCheck response status: {}", result.getStatusCode());
+        String body = result.getBody();
+        log.debug("deleteInstanceHealthCheck response body: {}", body);
+
+        if (!result.getStatusCode().is2xxSuccessful()) {
+            throw new IllegalStateException("Delete response different than 2xx:" + result.getStatusCode());
+        }
+    }
+
+    private HttpEntity<?> getHttpEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        List<MediaType> acceptableMediaTypes = new ArrayList<>();
+        acceptableMediaTypes.add(MediaType.APPLICATION_JSON);
+        headers.setAccept(acceptableMediaTypes);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return new HttpEntity<>(headers);
+    }
+
+    private class MulticloudApiUrl {
+
+        private String instanceId;
+        private final MulticloudConfiguration multicloudConfiguration;
+
+        MulticloudApiUrl(MulticloudConfiguration multicloudConfiguration1) {
+            this.multicloudConfiguration = multicloudConfiguration1;
+        }
+
+        String apiUrl() {
+            String instanceUri = multicloudConfiguration.getMulticloudUrl() + "/v1/instance/";
+            return instanceId.equals("") ? instanceUri : instanceUri + instanceId;
+        }
+
+        void setInstanceId(String instanceId) {
+            this.instanceId = instanceId;
+        }
+    }
+}
---
+++ so-cnf-adapter-application/src/main/java/org/onap/so/adapters/cnf/model/statuscheck/K8sRbInstanceGvk.java
@@ -0,0 +1,43 @@
+package org.onap.so.adapters.cnf.model.statuscheck;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(value = "true")
+public class K8sRbInstanceGvk {
+
+    @JsonProperty("Group")
+    private String group;
+
+    @JsonProperty("Kind")
+    private String kind;
+
+    @JsonProperty("Version")
+    private String version;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
