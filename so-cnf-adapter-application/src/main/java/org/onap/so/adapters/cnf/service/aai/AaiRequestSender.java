ba9953d34a076e49a0de7d60a5e51188ef8ddf90
Grzegorz Wielgosinski <g.wielgosins@samsung.com>
2021-09-07 08:39:46 +0200

    Add missing vnfId and vfModuleId parameters for aai create request Issue-ID: SO-3747
    Signed-off-by: Grzegorz Wielgosinski <g.wielgosins@samsung.com>
    Change-Id: Ib9eebaf2a851c84b2c7397fdc3045b360c286318

--- so-cnf-adapter-application/src/main/java/org/onap/so/adapters/cnf/service/aai/AaiRequestSender.java
+++ so-cnf-adapter-application/src/main/java/org/onap/so/adapters/cnf/service/aai/AaiRequestSender.java
@@ -21,10 +21,22 @@ class AaiRequestSender {
                 .build());
         String payload = gson.toJson(parseResult);
         getAaiClient().create(aaiUri, payload);
+
+        aaiUri = AAIUriFactory.createResourceUri(AAIFluentTypeBuilder.network()
+                .genericVnf(aaiRequest.getVnfId())
+                .vfModule(aaiRequest.getVfModuleId())
+                .build());
+        getAaiClient().create(aaiUri, payload);
     }
 
     void sendDeleteRequestToAai(AaiRequest aaiRequest) {
-        AAIResourceUri aaiUri = AAIUriFactory.createResourceUri(AAIFluentTypeBuilder.cloudInfrastructure()
+        AAIResourceUri aaiUri  = AAIUriFactory.createResourceUri(AAIFluentTypeBuilder.network()
+                .genericVnf(aaiRequest.getVnfId())
+                .vfModule(aaiRequest.getVfModuleId())
+                .build());
+        getAaiClient().delete(aaiUri);
+
+        aaiUri = AAIUriFactory.createResourceUri(AAIFluentTypeBuilder.cloudInfrastructure()
                 .cloudRegion(aaiRequest.getCloudOwner(), aaiRequest.getCloudRegion())
                 .tenant(aaiRequest.getTenantId())
                 .build());
