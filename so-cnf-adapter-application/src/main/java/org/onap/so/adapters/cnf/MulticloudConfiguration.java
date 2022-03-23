33b35cb92e78ccd1b54888d5e3ebfc36d9439eb3
Rama-Huawei <rama.subba.reddy.s@huawei.com>
2022-03-23 16:02:48 +0530

    Remove the unused imports
    Issue-ID: SO-3911
    
    Signed-off-by: Rama-Huawei <rama.subba.reddy.s@huawei.com>
    Change-Id: Ic83543005f4ad031f4bed1e59dc16ace7230ae24

--- so-cnf-adapter-application/src/main/java/org/onap/so/adapters/cnf/MulticloudConfiguration.java
+++ so-cnf-adapter-application/src/main/java/org/onap/so/adapters/cnf/MulticloudConfiguration.java
@@ -21,7 +21,6 @@
 package org.onap.so.adapters.cnf;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
